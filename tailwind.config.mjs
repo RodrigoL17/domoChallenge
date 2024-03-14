2962df5c218dff59984cdef7a01e7659ccf8ce5e
Rodrigo Lezama <cordolezama@gmail.com>
2024-03-14 13:06:55 -0300

    configuracion de colores y fuente

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary: '#ffffff',
+				secondary: '#9499a1',
+				bgPrimary: '#273244',
+				bgSecondary: '#4670f6',
+			}
+		},
 	},
 	plugins: [],
 }
